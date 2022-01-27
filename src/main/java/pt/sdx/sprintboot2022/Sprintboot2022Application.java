01ca2821f7cc2e7fcb540c279ddee9b01ffa0e72
cjsdiniz <67067863+cjsdiniz@users.noreply.github.com>
2022-01-27 11:12:04 +0000

    First Commit

---
+++ src/main/java/pt/sdx/sprintboot2022/Sprintboot2022Application.java
@@ -0,0 +1,13 @@
+package pt.sdx.sprintboot2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprintboot2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprintboot2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/pt/sdx/sprintboot2022/Sprintboot2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sdx.sprintboot2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprintboot2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
